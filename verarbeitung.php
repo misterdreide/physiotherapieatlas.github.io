41ab68c8d6e864f82d479b7cd281846df565b672
misterdreide <misterdreide@gmail.com>
2024-11-30 00:21:52 +0100

    Update verarbeitung.php
    delete

--- verarbeitung.php
+++ verarbeitung.php
@@ -1,15 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
 
-    if (!empty($name) && !empty($email)) {
-        echo "<h3>Vielen Dank für deine Nachricht, $name!</h3>";
-        echo "<p>Wir werden uns an $email melden.</p>";
-    } else {
-        echo "<p>Bitte fülle alle Felder aus.</p>";
-    }
-} else {
-    echo "<p>Ungültige Anforderung.</p>";
-}
-?>
